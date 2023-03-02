1f997365019a209bcfde1cc9cbbbfedbe8bdeb53
rijenth <rijenth93100@laposte.net>
2023-03-02 21:55:56 +0100

    Refacto: redirect Button into a loading button

---
+++ frontend/src/components/Authentication/LoadingButton.vue
@@ -0,0 +1,35 @@
+<template>
+  <button
+    class="bg-gray-800 mr-2 text-white p-2 px-3 rounded hover:bg-blue-800 text-center"
+    :disabled="isLoading"
+  >
+    <PulseLoader color="white" v-if="isLoading" size="10px" />
+
+    <p v-else>
+      {{ text }}
+    </p>
+  </button>
+</template>
+
+<script lang="ts">
+import PulseLoader from "vue-spinner/src/PulseLoader.vue";
+
+export default {
+  name: "LoadingButton",
+
+  components: {
+    PulseLoader,
+  },
+
+  props: {
+    text: {
+      type: String,
+      required: true,
+    },
+    isLoading: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
