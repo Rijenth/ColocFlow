d3e37fb667e7b1ae065514b7b304b0efcbad22f8
rijenth <rijenth93100@laposte.net>
2023-03-15 20:00:11 +0100

    [backend] Can update a charge relationship

---
+++ backend/app/Actions/UpdateChargeResourceAction.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Actions;
+
+use App\Models\Charge;
+use App\Models\User;
+
+class UpdateChargeResourceAction
+{
+    public function execute(Charge $charge, array $data): Charge
+    {
+        if (isset($data['attributes'])) {
+            $charge->fill($data['attributes']);
+        }
+
+        $charge->save();
+
+        if (isset($data['relationships'])) {
+            if (isset($data['relationships']['users'])) {
+                $this->updateChargeUserRelationship($charge, $data['relationships']['users']['data']);
+            }
+        }
+
+        return $charge->fresh();
+    }
+
+    public function updateChargeUserRelationship(Charge $charge, array $data): void
+    {
+        if (! empty($data)) {
+            $user = User::findOrFail($data['id']);
+
+            $newlyAffectedAmount = ($charge->users()->sum('amount') + $data['attributes']['amount']) - $charge->users()->find($user->id)->pivot->amount;
+
+            abort_if(
+                $newlyAffectedAmount > $charge->amount,
+                422,
+                'The amount of the charge cannot be exceeded.'
+            );
+
+            $charge->users()->syncWithoutDetaching(
+                [
+                    $user->id => [
+                        'amount' => $data['attributes']['amount'],
+                    ],
+                ]
+            );
+        }
+    }
+}
---
+++ backend/app/Http/Requests/UpdateChargeRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateChargeRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return $this->charge->colocation->owner->is($this->user()) || $this->charge->colocation->roommates->contains($this->user());
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'data.type' => 'required|string|in:charges',
+            'data.id' => 'required|integer|exists:charges,id',
+            'data.attributes' => 'array',
+            'data.relationships' => 'array',
+            'data.relationships.users' => 'array',
+            'data.relationships.users.data' => 'array',
+            'data.relationships.users.data.id' => 'integer|exists:users,id',
+            'data.relationships.users.data.type' => 'string|in:users',
+            'data.relationships.users.data.attributes' => 'array|required_with:data.relationships.users.data',
+            'data.relationships.users.data.attributes.amount' => 'numeric|min:0|required_with:data.relationships.users.data.attributes',
+        ];
+    }
+}
