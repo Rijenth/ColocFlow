5c0978829d6e351a1e0699e7e22b2cfb3359ad86
rijenth <rijenth93100@laposte.net>
2023-03-19 13:51:34 +0100

    [frontend] implement type check on eslint

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -9,8 +9,24 @@ module.exports = {
     "@vue/eslint-config-typescript",
     "@vue/eslint-config-prettier",
   ],
-  ignorePatterns: ["*.config.js"],
+  ignorePatterns: [
+    "*.config.js",
+    "vite.config.ts",
+    ".eslintrc.cjs",
+  ],
   parserOptions: {
     ecmaVersion: "latest",
+    parser: "@typescript-eslint/parser",
+    project: "./tsconfig.json"
+  },
+  rules: {
+    "@typescript-eslint/no-explicit-any": "warn",
+    "@typescript-eslint/no-unused-vars": "warn",
+    "@typescript-eslint/no-empty-function": "warn",
+    "@typescript-eslint/no-empty-interface": "warn",
+    "@typescript-eslint/no-var-requires": "warn",
+    "@typescript-eslint/no-non-null-assertion": "warn",
+    "@typescript-eslint/no-unsafe-assignment": "error",
+    "@typescript-eslint/no-unsafe-member-access": "error",
   },
 };
